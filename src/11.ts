0817dcc01bec1c80c951fc5a48f97dc19648180b
Per Ersson <per.ersson@gmail.com>
2021-12-12 23:01:12 +0100

    Day 11.1

---
+++ src/11.ts
@@ -0,0 +1,74 @@
+export { };
+
+// https://adventofcode.com/2021/day/11
+
+const values: readonly number[][] = `
+8258741254
+3335286211
+8468661311
+6164578353
+2138414553
+1785385447
+3441133751
+3586862837
+7568272878
+6833643144
+`
+    .split('\n')
+    .filter((v) => v)
+    .map((l) => l.split('').map((v) => +v));
+
+const evolveFlash = (vs: readonly number[][]): number[][] => {
+    const res: number[][] = [];
+    vs.forEach((r) => res.push(Object.assign([], r)));
+
+    const deltas = [
+        { dc: -1, dr: -1},
+        { dc: 0, dr: -1},
+        { dc: 1, dr: -1},
+        { dc: -1, dr: 0},
+        { dc: 1, dr: 0},
+        { dc: -1, dr: 1},
+        { dc: 0, dr: 1},
+        { dc: 1, dr: 1},
+    ]
+
+    const inrange = (r: number, c: number): boolean => r >= 0 && r < res.length && c >= 0 && c < res[r].length;
+
+    let more = true;
+    while (more) {
+        more = false;
+        res.map((r, rix) => {
+            r.map((c, cix) => {
+                if (c > 9) {
+                    deltas.forEach((d) => {
+                        const ri = rix + d.dr;
+                        const ci = cix + d.dc;
+                        if (inrange(ri, ci)) {
+                            if (res[ri][ci] > 0) {
+                                more = true;
+                                res[ri][ci] += 1;
+                            }
+                        }
+                    })
+                    res[rix][cix] = 0;
+                }
+            })
+        })
+    }
+    return res;
+}
+const evolveStep = (v: readonly number[][]): number[][] => {
+    return evolveFlash(v.map((r) => r.map((c) => c+1)));
+}
+
+let v = values;
+let totFlashes = 0;
+
+for(let i=0 ; i<=100 ; ++i) {
+    const flashes = v.map((r) => r.filter((v) => v === 0).length).reduce((a, b) => a + b, 0);
+    totFlashes += flashes;
+    console.log(i, flashes, totFlashes);
+    v = evolveStep(v);
+
+}
