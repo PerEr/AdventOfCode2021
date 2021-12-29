b3bae0ee562e16d655e3b4226fb7e2da2bef6d30
Per Ersson <per.ersson@gmail.com>
2021-12-29 22:02:42 +0100

    Day 17.1 simplified

--- src/17.ts
+++ src/17.ts
@@ -7,50 +7,19 @@ const target = {
     x1: 330, y1: -50,
 }
 
-const target2 = {
-    x0: 20, y0: -10,
-    x1: 30, y1: -5,
-}
 
-type HitType = 'Over' | 'Under' | 'Hit' | 'ThroughX'| 'ThroughY';
-
-interface Result {
-    maxY: number;
-    hit: HitType;
-}
-
-const simulateShot = (dx: number, dy: number): Result => {
-    let oldX = 0;
+const simulateShot = (dx: number, dy: number): number | undefined => {
     let x = 0;
-    let oldY = 0;
     let y = 0;
     let maxY = y;
     do {
         maxY = Math.max(y, maxY);
         if (x >= target.x0 && x <= target.x1 && y <= target.y1 && y >= target.y0) {
             // Hit
-            return {
-                maxY,
-                hit: 'Hit',
-            };
-        } else if (x > target.x1 && oldX < target.x0) {
-            return {
-                maxY,
-                hit: 'ThroughX',
-            };
-        } else if (y < target.y0 && oldY > target.y1) {
-            return {
-                maxY,
-                hit: 'ThroughY',
-            };
+            return maxY;
         } else if (y < target.y0) {
-            return {
-                maxY,
-                hit: x < target.x0 ? 'Under' : 'Over',
-            };
+            return;
         }
-        oldX = x;
-        oldY = y;
         x += dx;
         y += dy;
 
@@ -62,52 +31,17 @@ const simulateShot = (dx: number, dy: number): Result => {
 let dx = 17;
 let dy = -4;
 
-let best = -100;
-let more = 100;
-
-while (--more) {
-    const res = simulateShot(dx, dy);
-    console.log(dx, dy, res, best);
-    switch (res.hit) {
-        case 'ThroughX':
-            dx -= 1;
-            break;
-        case 'ThroughY':
-            dy -= 1;
-            break;
-        case 'Under':
-            dx += 1;
-        break;
-        case 'Over':
-            dy += 1;
-            dx -= 1;
-            break;
-        case 'Hit':
-            if (res.maxY > best) {
-                best = res.maxY;
-                more = 100;
-            }
-            dy += 1;
-            break;
-    }
-}
-
-console.log('Best:', best);
-
 const MAX = 250;
-best = 0;
+let best = 0;
 
 for (let dx = 0 ; dx<MAX ; ++dx) {
 
     for (let dy = 0 ; dy<MAX ; ++dy) {
         const res = simulateShot(dx, dy);
-        if (res.hit === 'Hit') {
-            if (res.maxY > best) {
-                console.log(dx, dy, res);
-                best = res.maxY;
-            }
+        if (res  && res > best) {
+            best = res;
         }
     }
 }
 
-console.log('Best2:', best);
+console.log('Max Y:', best);
